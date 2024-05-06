cacbf0bd4fbbe0bc532f7fbbd8237deb1c3ac7bd
Antoine Kaufmann <antoinek@mpi-sws.org>
2024-05-06 10:54:43 +0200

    remove old docker scripts

--- docker/run_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run --rm -it \
-    -p8888:8888 \
-    --device=/dev/kvm \
-    simbricks/labs
--- docker/run_dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker run --rm -it \
-    -p8888:8888 \
-    -v `pwd`:/simbricks/labs/ \
-    --device=/dev/kvm \
-    simbricks/labs
